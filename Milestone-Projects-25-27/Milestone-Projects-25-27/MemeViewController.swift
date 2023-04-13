888ba70560ab632ee079affec29b081f96ce8338
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-04-13 13:37:28 +0200

    feature challeng

---
+++ Milestone-Projects-25-27/Milestone-Projects-25-27/MemeViewController.swift
@@ -0,0 +1,130 @@
+//
+//  MemeViewController.swift
+//  Milestone-Projects-25-27
+//
+//  Created by Евгения Зорич on 13.04.2023.
+//
+
+import UIKit
+
+class MemeViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: UIImage?
+    var picture: UIImage?
+    var topText: String = ""
+    var bottomText: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image  = imageToLoad
+            
+            picture = imageToLoad
+            
+        }
+        createMemeFirst()
+    }
+    
+    func createMemeFirst() {
+        let ac = UIAlertController(title: "Create a meme", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: createMeme))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func createMemeSecond() {
+        let ac = UIAlertController(title: "Write a text for the bottom of the meme", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Ok", style: .default) { [self]_ in
+            self.bottomText = ac.textFields?[0].text ?? ""
+            createBottomMeme(text: bottomText)
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func createMeme(action: UIAlertAction) {
+        
+        let ac = UIAlertController(title: "Write a text for the top of the meme", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Ok", style: .default) { [self]_ in
+            self.topText = ac.textFields?[0].text ?? ""
+            createTopMeme(text: topText)
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    
+    func createTopMeme(text: String) {
+        let width = imageView.image?.size.width ?? 200
+        let height = imageView.image?.size.height ?? 200
+
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
+        
+        let img = renderer.image { ctx in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 20), .backgroundColor: UIColor.white,
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let mouse = imageView.image
+            mouse?.draw(at: CGPoint(x: 0, y: 0))
+            
+            let string = text
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            
+            attributedString.draw(with: CGRect(x: width * 0.01, y: height * 0.01, width: width, height: height), options: .usesLineFragmentOrigin, context: nil)
+        }
+        imageView.image = img
+        
+        createMemeSecond()
+    }
+    
+    func createBottomMeme(text: String) {
+        let width = imageView.image?.size.width ?? 512
+        let height = imageView.image?.size.height ?? 512
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
+        
+        let img = renderer.image { ctx in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 30), .backgroundColor: UIColor.white,
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let mouse = imageView.image
+            mouse?.draw(at: CGPoint(x: 0, y: 0))
+            
+            let string = text
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            
+            attributedString.draw(with: CGRect(x: width * 0.1, y: height * 0.8, width: width, height: height), options: .usesLineFragmentOrigin, context: nil)
+        }
+        imageView.image = img
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        var imageArray: [Any] = [image]
+                if let imageText = selectedImage {
+                    imageArray.append(imageText)
+                }
+
+        let vc = UIActivityViewController(activityItems: imageArray, applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
--- Milestone-Projects-25-27/Milestone-Projects-25-27/ViewController.swift
+++ Milestone-Projects-25-27/Milestone-Projects-25-27/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var images = [UIImage]()
+    
+    var imageMeme: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Meme generator"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        return cell
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+        
+        let ac = UIAlertController(title: "Let's go", message: "Now click on the picture", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+        
     }
 
-
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+                if let vc = storyboard?.instantiateViewController(withIdentifier: "Meme") as? MemeViewController {
+                    vc.selectedImage = images[indexPath.item]
+                    navigationController?.pushViewController(vc, animated: true)
+                }
+            
+    }
+    
+    @objc func importPicture() {
+        let ac = UIAlertController(title: "Download a picture", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: pickPicture))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func pickPicture(action: UIAlertAction) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 }
-
